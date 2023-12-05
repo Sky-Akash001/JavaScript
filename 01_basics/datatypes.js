afc582da9a504c6a9ec72106ff5d62166500708c
Sky-Akash001 <71833206+Sky-Akash001@users.noreply.github.com>
2023-12-05 03:15:33 +0000

    commit changes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all code as newer version of javascript
+
+//alert (3+2) //we are using node js, not browser
+
+/*
+Data Types
+number =>2 t0 power 53
+bigint
+string
+boolean
+null =>representation of empty value
+undefined
+symbol =>used for quiqueness
+*/
+
+//object data types
+
+console.log(typeof "Akash")
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basics/test.js
@@ -0,0 +1,10 @@
+const accountId = 1233
+let name = "Akash Vardhan"
+
+let accountState;
+
+/* Prefer not to use var
+*/
+//name = "Sonu" //
+
+console.table([accountId,name, accountState])
