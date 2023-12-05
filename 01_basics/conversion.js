bf1284cddd870da367abb1556976e8442bb2cc2d
Sky-Akash001 <71833206+Sky-Akash001@users.noreply.github.com>
2023-12-05 03:28:17 +0000

    commit

---
+++ 01_basics/conversion.js
@@ -0,0 +1,13 @@
+let score = 0
+let valueInNumber = Number(score)
+
+// console.table([typeof score, typeof valueInNumber])
+// console.log(score, valueInNumber)
+
+//"33" => 33
+//"33a" =>NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
