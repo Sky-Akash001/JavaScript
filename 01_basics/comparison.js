5a0f92547b9c02b11b409245eca0c661f07d3a5d
Sky-Akash001 <71833206+Sky-Akash001@users.noreply.github.com>
2023-12-05 03:49:42 +0000

    commit

---
+++ 01_basics/comparison.js
@@ -0,0 +1,9 @@
+console.log(null>0) // false
+console.log(null == 0) // false
+console.log(null >= 0) // true
+
+//equality and comparison work differently, comparison convert null to 0 and then compare
+
+// === strict check
+console.log(2 === "2")
+console.log(2 == "2");
