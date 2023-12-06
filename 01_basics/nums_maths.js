b34ef23afb9712f0ec3e44d73d2043ed8129982d
Sky-Akash001 <71833206+Sky-Akash001@users.noreply.github.com>
2023-12-06 10:55:18 +0530

    Update nums_maths.js

--- 01_basics/nums_maths.js
+++ 01_basics/nums_maths.js
@@ -1,2 +1,24 @@
 const score  = new Number("20")
-console.log(typeof score, score)
+// console.log(typeof score, score)
+// console.log(score.toFixed(2));
+
+const otherNum = 123.89
+// console.log(otherNum.toFixed(1));
+// console.log(otherNum.toPrecision(2));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+// console.log(Number.);
+
+//********************************************8 */
+
+console.log(Math.abs(-5));
+console.log((Math.random()*10))
+
+//generating random value
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1) + min))
