6eba075904a975bd6389b1f8c4ceef925d987bce
Sky-Akash001 <71833206+Sky-Akash001@users.noreply.github.com>
2023-12-07 03:00:35 +0000

    commit

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,3 +1,23 @@
+const mySym = Symbol("Key1")
 const myObje = {
-    
+    name : "akash",
+    "full name" : "Akash Vardhan",
+    [mySym] : "mykey1"
 }
+
+// console.log(myObje)
+// Object.freeze(myObje)
+myObje.name = "Akash"
+// console.log(myObje);
+
+myObje.greeting = function(){
+    console.log("Welcome to this object")
+}
+
+console.log(myObje.greeting())
+
+myObje.greeting2 = function(){
+    console.log(`Welcome ${myObje["full name"]} to this object`)
+}
+
+myObje.greeting2()
