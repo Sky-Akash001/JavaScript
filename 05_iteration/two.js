c6b6f2778426fd8b8ca68b385bf405f0cf6e80c8
Sky-Akash001 <71833206+Sky-Akash001@users.noreply.github.com>
2023-12-14 06:10:02 +0000

    commit

---
+++ 05_iteration/two.js
@@ -0,0 +1,21 @@
+let i = 1;
+// console.log("Table of 2")
+while(i<=10){
+    // console.log(`2 * ${i} = ${2*i}`);
+    i++
+}
+
+myArray = ["Ram","Sam","Tam","Tommy"]
+let index=0;
+while(index<myArray.length){
+    // console.log(`Value at index ${index} is ${myArray[index]}`)
+    index++
+}
+
+let score = 0
+
+do{
+    console.log(`Score is ${score}`)
+    score+=2
+}
+while(score <=10);
